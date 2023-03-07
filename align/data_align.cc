781280bca7b7c0168df2c4473a23fa81b384bf0d
lihang <hang.li@dorabot.com>
2023-03-07 18:54:58 +0800

    数据对齐

---
+++ align/data_align.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+struct A {
+  char a1;
+  int a2;
+  double a3;
+};
+
+struct B {
+  short b1;
+  bool b2;
+  double b3;
+};
+
+int main() {
+  std::cout << "sizeof(A::a1)+sizeof(A::a2)+sizeof(A::a3)="
+            << sizeof(A::a1) + sizeof(A::a2) + sizeof(A::a3) << std::endl;
+  std::cout << "sizeof(B::b1)+sizeof(B::b2)+sizeof(B::b3)="
+            << sizeof(B::b1) + sizeof(B::b2) + sizeof(B::b3) << std::endl;
+  std::cout << "sizof(A):" << sizeof(A) << std::endl;
+  std::cout << "sizof(B):" << sizeof(B) << std::endl;
+  return 0;
+}
